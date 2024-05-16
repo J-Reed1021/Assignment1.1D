b6fa5d3e9c84978b4498e3d2bdadfb17d23c7ecc
joshu <joshu@REEDPC>
2024-05-16 14:54:47 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter the first number");
+int fNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number");
+int sNumber = Convert.ToInt32(Console.ReadLine());
+
+int result = fNumber / sNumber;
+Console.WriteLine("The result is " + fNumber/sNumber);
